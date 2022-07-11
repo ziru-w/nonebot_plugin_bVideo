0c62cf223d0ebe9860de49df10e2ea85b851c0bd
ziru-w <77319678+ziru-w@users.noreply.github.com>
2022-07-11 10:48:18 +0800

    更改目录

---
+++ README.md
@@ -0,0 +1,15 @@
+# nonebot_plugin_bVideo
+一个基于NoneBot2的插件，用于处理推送B站up主最新视频的插件
+
+
+A NoneBot2-based plugin for processing plugins that push the latest videos of the B station up master
+
+
+下载方法 pip install nonebot_plugin_bVideo
+
+
+/收录B站UP 个人空间链接 不要链接里?以后的部分 用于收录B站UP的mid，形如/收录B站UP https://space.bilibili.com/688379639
+
+/推送b站 返回收录的前6个up的视频标题加链接
+
+/添加B站推送 up主名字 /删除B站推送 up主名字 群聊发推送到该群，私聊推送到发送者 名字允许不一样，但只返回第一个包含结果，所以请尽量一样
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="nonebot_plugin_bVideo",
+    version="1.0.0",
+    author="wziru",
+    description="A NoneBot2-based plugin for processing plugins that push the latest videos of the B station up master.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ziru-w/nonebot_plugin_bVideo",
+    project_urls={
+        "Bug Tracker": "https://github.com/ziru-w/nonebot_plugin_bVideo/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=["nonebot_plugin_bVideo"],
+    python_requires=">=3.7",
+    install_requires = [
+        'nonebot2>=2.0.0b1',
+        'apscheduler>=3.8.1'
+    ]
+)
