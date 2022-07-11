c6dbfe0b7f7fef6b0207e200f6a2ce4f986e699a
ziru-w <77319678+ziru-w@users.noreply.github.com>
2022-07-11 15:28:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="nonebot_plugin_bVideo",
-    version="1.0.2",
+    version="1.1.0",
     author="wziru",
     description="A NoneBot2-based plugin for processing plugins that push the latest videos of the B station up master.",
     long_description=long_description,
